b7bc8b70284131cc3faf05f7d574cebb7108e16e
call me Sunil <155473464+Technical-sunil@users.noreply.github.com>
2024-03-02 23:09:44 +0545

    Update Ip-Info.sh

--- Ip-Info.sh
+++ Ip-Info.sh
@@ -4,6 +4,8 @@ toilet -f smblock "Ip-information" | lolcat
 echo "<👉----------------------------------👈>"
 echo -e "\033[0;32mIp Info-Gathering Tool" | lolcat
 echo "Tool Name: Ip info"
-echo -e "Creat_by-Sunil" | lolcat                       read -p "Enter Your Target Ip: " user_ip
-echo -e "\e[32;1m Gathering Ip_Info..."                 sleep 3s
+echo -e "Creat_by-Sunil" | lolcat 
+read -p "Enter Your Target Ip: " user_ip
+echo -e "\e[32;1m Gathering Ip_Info..."                 
+sleep 3s
 curl ip-api.com/$user_ip
